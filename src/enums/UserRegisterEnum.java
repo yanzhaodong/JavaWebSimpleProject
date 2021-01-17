0269f2a261d9be8c04612bfa93b55dc8ebefc814
ZhaodongYan1 <yanzhaodong@smartbi.com.cn>
2021-01-17 22:33:13 +0800

    updated

--- src/enums/UserRegisterEnum.java
+++ src/enums/UserRegisterEnum.java
@@ -6,8 +6,7 @@ public enum UserRegisterEnum {
 	USER_EMAIL_INVALID("注册邮箱格式不规范"),
 	USER_PASSWORDS_DISMATCH("密码不一致"),
 	USER_ALREADY_EXIST("用户已存在");
-	
-	
+
 	private String value;
 	
 	private UserRegisterEnum(String value) {
